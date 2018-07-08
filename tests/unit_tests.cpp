bb04f26584e580655670dbee1fc1d9b68f1aa17a
Ilan Olkies <ilanolkies@outlook.com>
2018-07-08 20:55:26 -0300

    Agregar test pila dos elementos con pop

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -309,3 +309,48 @@ TEST(test_pila, dos_elementos) {
     ASSERT_EQ(p->getStack().top(), 20);
     ASSERT_EQ(p->getStack().empty(), false);
 }
+
+TEST(test_pila, dos_elementos_pop) {
+    Pila *p = new Pila;
+
+    ASSERT_EQ(p->size(), 0);
+    ASSERT_EQ(p->top(), 0);
+    ASSERT_EQ(p->getStack().size(), 0);
+    ASSERT_EQ(p->getStack().empty(), true);
+    ASSERT_EQ(p->pop(), 0);
+
+    p->push(10);
+
+    ASSERT_EQ(p->size(), 1);
+    ASSERT_EQ(p->top(), 10);
+    ASSERT_EQ(p->getStack().size(), 1);
+    ASSERT_EQ(p->getStack().top(), 10);
+    ASSERT_EQ(p->getStack().empty(), false);
+
+    p->push(20);
+
+    ASSERT_EQ(p->size(), 2);
+    ASSERT_EQ(p->top(), 20);
+    ASSERT_EQ(p->getStack().size(), 2);
+    ASSERT_EQ(p->getStack().top(), 20);
+    ASSERT_EQ(p->getStack().empty(), false);
+
+    int pop = p->pop();
+    ASSERT_EQ(pop, 20);
+
+    ASSERT_EQ(p->size(), 1);
+    ASSERT_EQ(p->top(), 10);
+    ASSERT_EQ(p->getStack().size(), 1);
+    ASSERT_EQ(p->getStack().empty(), false);
+    ASSERT_EQ(p->getStack().top(), 10);
+
+    pop = p->pop();
+    ASSERT_EQ(pop, 10);
+
+    ASSERT_EQ(p->size(), 0);
+    ASSERT_EQ(p->top(), 0);
+    ASSERT_EQ(p->getStack().size(), 0);
+    ASSERT_EQ(p->getStack().empty(), true);
+
+    ASSERT_EQ(p->pop(), 0);
+}
