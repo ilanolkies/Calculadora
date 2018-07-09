2de3ec5ba03b6afcd4967a8b3dc77e1a7192040c
Ilan Olkies <ilanolkies@outlook.com>
2018-07-08 21:04:13 -0300

    Agregar test pila aliasing

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -354,3 +354,43 @@ TEST(test_pila, dos_elementos_pop) {
 
     ASSERT_EQ(p->pop(), 0);
 }
+
+TEST(test_pila, un_elemento_aliasing) {
+    Pila *p = new Pila;
+
+    ASSERT_EQ(p->size(), 0);
+    ASSERT_EQ(p->top(), 0);
+    ASSERT_EQ(p->getStack().size(), 0);
+    ASSERT_EQ(p->getStack().empty(), true);
+    ASSERT_EQ(p->pop(), 0);
+
+    p->push(10);
+
+    auto s = p->getStack();
+
+    ASSERT_EQ(p->size(), 1);
+    ASSERT_EQ(p->top(), 10);
+
+    ASSERT_EQ(s.size(), 1);
+    ASSERT_EQ(s.empty(), false);
+    ASSERT_EQ(s.top(), 10);
+
+    int pop = p->pop();
+    ASSERT_EQ(pop, 10);
+
+    ASSERT_EQ(p->size(), 0);
+    ASSERT_EQ(p->top(), 0);
+
+    ASSERT_EQ(s.size(), 1);
+    ASSERT_EQ(s.empty(), false);
+    ASSERT_EQ(s.top(), 10);
+
+    int s_top = s.top();
+    s.pop();
+
+    ASSERT_EQ(p->size(), 0);
+    ASSERT_EQ(p->top(), 0);
+
+    ASSERT_EQ(s.size(), 0);
+    ASSERT_EQ(s.empty(), true);
+}
